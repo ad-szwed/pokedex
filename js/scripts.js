0d6844bd93a3d454df04313d5f050d3e74126fb3
ad-szwed <ad-szwed@users.noreply.github.com>
2021-01-04 13:39:44 +0000

    JS excersie 1.2
    Created an empty array
    Added 3 objects to that array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-alert('Hello world');
-// one line comment
-/* many
-lines
-comment  */
-let favoriteFood = 'a value of my choice'
-document.write(favoriteFood)
+let pokemonList = []
+
+let poke1 = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  type: ['grass','posion']
+}
+
+let poke2 = {
+  name: 'Weedle',
+  height: 0.3,
+  type: ['bug','posion']
+}
+
+let poke3 = {
+  name: 'Nidoqueen',
+  height: 1.3,
+  type: ['ground','posion']
+}
+
+pokemonList.push(poke1, poke2, poke3)
