1adddbdd1fd5472c5f12e0ae8beb1a3c59cfe822
ad-szwed <ad-szwed@users.noreply.github.com>
2021-01-12 12:00:54 +0000

    1.2.2 task
    Wrapped the pokemon list in IIFE
    Added option to see the list and add new pokemons
    Adding new pokemons reuires:
    Correct format,
    Unique entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,43 @@
 // pokemons list build of name, height, type
-let pokemonList = [
-{name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
-{name: 'Weedle', height: 0.3, type: ['bug','posion']},
-{name: 'Nidoqueen', height: 1.3, type: ['ground','posion']}
-];
 
-//forEach iteration to show if pokemon is big or small
-pokemonList.forEach(function(pokemon){
-  if(pokemon.height > 1){
-    document.write('<p>' + pokemon.name + '\'s height is ' + pokemon.height + 'm - '  + "that's a big boy!</p>")
-  }else {
-    document.write('<p>' + pokemon.name + '\'s height is ' + pokemon.height + 'm - '  + "that's a small boy!</p>")
-  } 
-});
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
+    {name: 'Weedle', height: 0.3, type: ['bug','posion']},
+    {name: 'Nidoqueen', height: 1.3, type: ['ground','posion']}
+    ];
+    
+
+    // valid keys instruction for add(pokemon)
+    const validKeys = ['name', 'height', 'type']
+    function check(obj, arr) {
+      return Object.keys(obj).every(e => arr.includes(e));
+    }
+
+    function add(pokemon){
+         
+        return (typeof pokemon === 'object' &&  // check if input is object AND
+        pokemon !== null && // not empty AND
+        check(pokemon, validKeys) == true && // valid keys are used AND
+        pokemonList.indexOf(pokemon.name) !== -1 ? //  not in the pokemonList already
+        pokemonList.push(pokemon) : "error, invalid or non-unique input, please follow this format \n {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']}")    
+    }
+    //show pokemons list
+    function getAll(){
+      return pokemonList;
+    }
+    // add new pokemons to the array
+    return {
+      add: add,
+      getAll: getAll
+    }
+})();
+
+// display pokemons with their height
+function showEm() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    document.write('<p>' + pokemon.name + '\'s height is ' + pokemon.height + 'm </p>')
+  });document.write('=========END OF LIST=========')
+}
+
+showEm()
