caecd143b5c99cca748c0dc2b2914a586f231c65
ad-szwed <ad-szwed@users.noreply.github.com>
2021-01-15 15:14:39 +0000

    1.6.2 task Event Listener
    Added a listener displaying names of pokemons when clicked on the button

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ let pokemonRepository = (function () {
     return Object.keys(obj).every(e => arr.includes(e));
   }
 
+  // adding pokemons to the repository
   function add(pokemon){    
       return (typeof pokemon === 'object' &&  // check if input is object AND
       pokemon !== null && // not empty AND
@@ -22,17 +23,27 @@ let pokemonRepository = (function () {
       pokemonList.push(pokemon) : "error, invalid or non-unique input, please follow this format \n {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']}")    
   }
 
+  function showDetails(pokemon){
+    console.log(pokemon)
+  };
+
+  // adding buttons for UI
   function addListItem(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add("button")
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
+    button.classList.add('button')
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    
+    // button click listener
+    button.addEventListener('click', function(event){
+      showDetails(pokemon.name)
+    })
   }
 
-  //show pokemons list
+    //show pokemons list
   function getAll(){
     return pokemonList;
   }
@@ -45,9 +56,8 @@ let pokemonRepository = (function () {
   }
 })();
 
-console.log(pokemonRepository.getAll());
-
-// display pokemons with their height
+// display pokemons
 pokemonRepository.getAll().forEach(function(pokemon){
   pokemonRepository.addListItem(pokemon)
 });
+
