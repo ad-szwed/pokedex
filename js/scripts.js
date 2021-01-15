1957afee3f6e777435f60fa4a120bdbe83eba697
ad-szwed <ad-szwed@users.noreply.github.com>
2021-01-15 12:04:03 +0000

    1.6.1 task DOM Manipulation Practice
    Added placeholder visuals in style.css
    created addListItem() as per task requirements in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,39 +5,49 @@ let pokemonRepository = (function () {
     {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']},
     {name: 'Weedle', height: 0.3, type: ['bug','posion']},
     {name: 'Nidoqueen', height: 1.3, type: ['ground','posion']}
-    ];
+  ];
     
 
-    // valid keys instruction for add(pokemon)
-    const validKeys = ['name', 'height', 'type']
-    function check(obj, arr) {
-      return Object.keys(obj).every(e => arr.includes(e));
-    }
+  // valid keys instruction for add(pokemon)
+  const validKeys = ['name', 'height', 'type']
+  function check(obj, arr) {
+    return Object.keys(obj).every(e => arr.includes(e));
+  }
 
-    function add(pokemon){
-         
-        return (typeof pokemon === 'object' &&  // check if input is object AND
-        pokemon !== null && // not empty AND
-        check(pokemon, validKeys) == true && // valid keys are used AND
-        pokemonList.indexOf(pokemon.name) !== -1 ? //  not in the pokemonList already
-        pokemonList.push(pokemon) : "error, invalid or non-unique input, please follow this format \n {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']}")    
-    }
-    //show pokemons list
-    function getAll(){
-      return pokemonList;
-    }
-    // add new pokemons to the array
-    return {
-      add: add,
-      getAll: getAll
-    }
+  function add(pokemon){    
+      return (typeof pokemon === 'object' &&  // check if input is object AND
+      pokemon !== null && // not empty AND
+      check(pokemon, validKeys) == true && // valid keys are used AND
+      pokemonList.indexOf(pokemon.name) !== -1 ? //  not in the pokemonList already
+      pokemonList.push(pokemon) : "error, invalid or non-unique input, please follow this format \n {name: 'Bulbasaur', height: 0.7, type: ['grass','posion']}")    
+  }
+
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button")
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+
+  //show pokemons list
+  function getAll(){
+    return pokemonList;
+  }
+
+  // add new pokemons to the array
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  }
 })();
 
-// display pokemons with their height
-function showEm() {
-  pokemonRepository.getAll().forEach(function(pokemon){
-    document.write('<p>' + pokemon.name + '\'s height is ' + pokemon.height + 'm </p>')
-  });document.write('=========END OF LIST=========')
-}
+console.log(pokemonRepository.getAll());
 
-showEm()
+// display pokemons with their height
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon)
+});
